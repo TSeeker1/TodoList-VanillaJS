8da3eee3d3e4d72c16aaf1f155321255b01f44ea
Tair <tairwork24@gmail.com>
2022-06-13 16:33:12 +0600

    added delete task button

--- todoList.js
+++ todoList.js
@@ -16,8 +16,16 @@ function addList(value) {
 
    // * add li to the list
    todoOl.append(li);
+
+   // *creating delete button
+   let btn = createCertainDeleteBtn();
+
+   btn.addEventListener("click", clearTask);
+
+   li.append(btn);
 }
 
+// * принять название для задачи
 function todoApp() {
    let str = input.value.trim();
    if (str.length == 0) return;
@@ -27,16 +35,36 @@ function todoApp() {
    input.value = "";
 }
 
+// * очистить список
 function clearList() {
    todoOl.innerHTML = "";
 }
 
+// * включить цифры или нет
 function toggleNum() {
    if (todoOl.style.listStyle == "none") {
       todoOl.style.listStyle = "decimal inside";
    } else {
       todoOl.style.listStyle = "none";
    }
+}
+
+// *creating delete certain button
+function createCertainDeleteBtn() {
+   let btn = document.createElement("button");
+   btn.innerHTML = "X";
+   btn.classList.add("deleteCertainBtn");
+
+   return btn;
+}
+
+//* отдельная кнопка для очиски
+function clearTask(event) {
+   let li = event.target.closest("li");
+
+   if (!todoOl.contains(li)) return;
+
+   li.remove();
 }
 
 submit.addEventListener("click", todoApp);
